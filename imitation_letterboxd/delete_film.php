f925b44eaf59ec3574fe0896ba09ef9be6dd5975
IBookki <147388746+IBookki@users.noreply.github.com>
2024-11-03 11:56:31 +0100

    Update delete_film.php

--- imitation_letterboxd/delete_film.php
+++ imitation_letterboxd/delete_film.php
@@ -1,18 +1,15 @@
 <?php
 session_start();
 require 'config/auth.php';
-verifierAdmin();  // Vérifie que l'utilisateur est bien un administrateur
+verifierAdmin();
 require 'config/db.php';
 
-// Vérifie si l'ID est présent dans l'URL
 if (isset($_GET['id'])) {
     $id = $_GET['id'];
 
-    // Prépare la requête de suppression
     $stmt = $pdo->prepare("DELETE FROM films WHERE id = :id");
 
     try {
-        // Exécute la requête de suppression
         $stmt->execute(['id' => $id]);
         echo "<p>Film supprimé avec succès.</p>";
     } catch (PDOException $e) {
@@ -22,7 +19,6 @@ if (isset($_GET['id'])) {
     echo "<p>Aucun identifiant de film fourni.</p>";
 }
 
-// Redirige vers la liste des films après suppression
 header("Location: films.php");
 exit();
 ?>
